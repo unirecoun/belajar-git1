1d6d9fc4033494d57abff11a9d8935a91c70860f
REYFALDO WINDRYANSYAH <aldo1234bawel@gmail.com>
2025-02-19 12:47:38 +0700

    Add files via upload

---
+++ Tugas/conditions and loops/break.dart
@@ -0,0 +1,44 @@
+void main() {
+  var noOfMoneth = 5;
+  switch (noOfMoneth) {
+    case 1:
+      print("Bulan ini adalah bulan januari.");
+      break;
+    case 2:
+      print("Bulan ini adalah bulan febuari.");
+      break;
+    case 3:
+      print("Bulan ini adlah bulan maret.");
+      break;
+    case 4:
+      print("Bulan ini adlah bulan april.");
+      break;
+    case 5:
+      print("Bulan ini adlah bulan mei.");
+      break;
+    case 6:
+      print("Bulan ini adlah bulan juni.");
+      break;
+    case 7:
+      print("Bulan ini adlah bulan juli.");
+      break;
+    case 8:
+      print("Selected month is August.");
+      break;
+    case 9:
+      print("Selected month is September.");
+      break;
+    case 10:
+      print("Selected month is October.");
+      break;
+    case 11:
+      print("Selected month is November.");
+      break;
+    case 12:
+      print("Selected month is December.");
+      break;
+    default:
+      print("Invalid month.");
+      break;
+  }
+}
